1c02146201593d309b68af0cdfdb295c9fe7f62e
PostmanPat2011 <46075957+PostmanPat2011@users.noreply.github.com>
2024-11-13 21:00:49 -0500

    Update start.sh

--- cmds/start.sh
+++ cmds/start.sh
@@ -18,13 +18,11 @@ systemctl stop gdm.service
 echo 0 > /sys/class/vtconsole/vtcon0/bind
 # Some machines might have more than 1 virtual console. Add a line for each corresponding VTConsole
 # echo 0 > /sys/class/vtconsole/vtcon1/bind
+
 # Unbind EFI-Framebuffer
 #echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
 
-# Avoid a race condition by waiting a couple of seconds. This can be calibrated to be shorter or longer if required for your system
-
-# sleep 5
-# Unload all Nvidia drivers
+# Unload all drivers
 modprobe -r nouveau
 # Unbind the GPU from display driver
 virsh nodedev-detach $VIRSH_GPU_VIDEO
